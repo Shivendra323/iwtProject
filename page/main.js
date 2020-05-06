f2a5f9f5d0e46d2a212091b204f70c37d82c0d1c
Shivendra323 <64783953+Shivendra323@users.noreply.github.com>
2020-05-06 21:26:22 +0530

    Update main.js

--- page/main.js
+++ page/main.js
@@ -14,6 +14,16 @@ var turnReady;
 var pcConfig = {
   'iceServers': [{
      'urls': 'stun:bn-turn1.xirsys.com'
+   },
+   {
+     'urls': 'turn:bn-turn1.xirsys.com:80?transport=udp',
+     'credential':'a1693180-8f9a-11ea-8742-9646de0e6ccd',
+     'username' : '2upAxkES280ExSJQCE2gkzJUkR_9fXUk88poCqtRDDH72A6h_VHAQECPQX1eIhQnAAAAAF6ytrhTaGl2ZW5kcmEzMjM='
+     },
+   {
+     'urls': 'turns:bn-turn1.xirsys.com:443?transport=tcp',
+     'credential':'a1693180-8f9a-11ea-8742-9646de0e6ccd',
+     'username' : '2upAxkES280ExSJQCE2gkzJUkR_9fXUk88poCqtRDDH72A6h_VHAQECPQX1eIhQnAAAAAF6ytrhTaGl2ZW5kcmEzMjM='
    }]
 };
 
